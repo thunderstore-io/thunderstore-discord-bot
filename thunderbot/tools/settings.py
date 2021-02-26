d01ddcc837d0835f78b82248b8bcfed4955e9aef
SirQuacker <49299629+SirQuacker@users.noreply.github.com>
2021-02-26 18:36:10 +1100

    - updated deprecate to new format - added settings module for global storage of configs - added new mod search function - added error message on failed command calls - added more info to help command

--- thunderbot/tools/settings.py
+++ thunderbot/tools/settings.py
@@ -7,10 +7,12 @@ load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 API_KEY = os.getenv("THUNDERSTORE_API_KEY_ID")
 URL = os.getenv("THUNDERSTORE_API_URL")
+
 try:
     USER_KEY = base64_decode(os.getenv("THUNDERSTORE_API_SECRET"))
 except TypeError:
     print("Env var THUNDERSTORE_API_SECRET not found or cant decode")
+
 USER_ALGORITHM = os.getenv("THUNDERSTORE_API_ALGORITHM")
 PACKAGE_REFRESH_TIME = os.getenv("PACKAGE_REFRESH_TIME")
 
