1d3915aa5137114850355cf43e07b748fa14c905
SirQuacker <49299629+SirQuacker@users.noreply.github.com>
2021-03-07 19:48:04 +1100

    add try catch for thunderstore refresh

--- thunderbot/commands/thunderstore.py
+++ thunderbot/commands/thunderstore.py
@@ -27,15 +27,19 @@ class Thunderstore(commands.Cog):
         await self.client.change_presence(activity=discord.Game(f'Refreshing Packages'))
         total = 0
         header = {"content-type": "application/json"}
-        for ser in settings.SER_PREF:
-            response = requests.get(settings.SER_PREF[ser][1] + "/v1/package/", headers=header)
-            package_list = json.loads(response.content)
-            settings.SER_PREF[ser][2] = package_list
-            settings.SER_PREF[ser][3] = []
+        try:
+            for ser in settings.SER_PREF:
+                response = requests.get(settings.SER_PREF[ser][1] + "/v1/package/", headers=header)
+                package_list = json.loads(response.content)
+                settings.SER_PREF[ser][2] = package_list
+                settings.SER_PREF[ser][3] = []
 
-            for d in package_list:
-                settings.SER_PREF[ser][3].append(d["full_name"])
-            total += len(settings.SER_PREF[ser][3])
+                for d in package_list:
+                    settings.SER_PREF[ser][3].append(d["full_name"])
+                total += len(settings.SER_PREF[ser][3])
+        except:
+            print("Error with package refresh")
+            await self.client.change_presence(activity=discord.Game(f'Cannot connect to Thunderstore'))
 
         await self.client.change_presence(activity=discord.Game(f'{total} total packages'))
 
